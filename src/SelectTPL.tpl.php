736265ca06947060d88208918b1311a169c1ee55
夏琳泰 <xltxlm@qq.com>
2017-02-10 14:22:10 +0800

    下拉框可以搜索

--- src/SelectTPL.tpl.php
+++ src/SelectTPL.tpl.php
@@ -1,5 +1,23 @@
+<?php
+if (!defined(__FILE__)) {
+    ?>
+    <!-- Select2 -->
+    <link rel="stylesheet" href="/static/css/select2.min.css">
+    <script  src="/static/js/select2.min.js"></script>
+    <script>
+        $(function () {
+            //Initialize Select2 Elements
+            $(".select2").select2();
+        });
+    </script>
+    <?php
+    define(__FILE__, true);
+}
+?>
 <?php /** @var \xltxlm\h5skin\SelectTPL $this */ ?>
-<select class="<?=$this->getClassName()?>" name="<?= $this->getName() ?>"  <?= $this->getVmodel() ?> <?= $this->getVOnChange() ?>  id="<?= $this->getId() ?>" title="<?= $this->getName() ?>">
+<select class="<?= $this->getClassName() ?> select2"
+        name="<?= $this->getName() ?>" <?= $this->getVmodel() ?> <?= $this->getVOnChange() ?> id="<?= $this->getId() ?>"
+        title="<?= $this->getName() ?>">
     <?php foreach ($this->getOptions() as $showText => $value) { ?>
         <option value="<?= $value ?>"
                 <?php if ($this->getDefault() == $value){ ?>selected<?php } ?>><?= $showText ?></option>
