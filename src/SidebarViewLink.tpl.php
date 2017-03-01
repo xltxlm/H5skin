d8ae05735a8683e1b954988e1d3b22791eaace16
夏琳泰 <xltxlm@qq.com>
2017-03-01 13:19:45 +0800

    优化项目

--- src/SidebarViewLink.tpl.php
+++ src/SidebarViewLink.tpl.php
@@ -1,8 +1,8 @@
 <?php /** @var  \xltxlm\h5skin\SidebarViewLink $this */
 ?>
-<li class="<<?= '?' ?>= in_array(LoadClass::$runClass,<?= json_encode($this->getHighlightClass(), JSON_UNESCAPED_UNICODE) ?>) ? 'active' : '' <?= '?' ?>>">
+<li class="<<?= '?' ?>= (!$_GET['class'] && in_array(LoadClass::$runClass,<?= json_encode($this->getHighlightClass(), JSON_UNESCAPED_UNICODE) ?>) || ($_GET['class'] && $_GET['class'] == '<?=$this->getArgs()['class']?>') ) ? 'active' : '' <?= '?' ?>>">
     <a href="<<?= '?' ?>= call_user_func([<?= $this->getClassName() ?>::class, 'urlNoFollow'], json_decode('<?= json_encode($this->getArgs(), JSON_UNESCAPED_UNICODE) ?>',true)) <?= '?' ?>>">
-        <<?= '?' ?>php if (in_array(LoadClass::$runClass,<?= json_encode($this->getHighlightClass(), JSON_UNESCAPED_UNICODE) ?>)) { <?= '?' ?>>
+        <<?= '?' ?>php if ( !$_GET['class'] && in_array(LoadClass::$runClass,<?= json_encode($this->getHighlightClass(), JSON_UNESCAPED_UNICODE) ?>)  || ($_GET['class'] && $_GET['class'] == '<?=$this->getArgs()['class']?>') ) { <?= '?' ?>>
             <i class="fa fa-circle-o text-red"></i>
             <<?= '?' ?>php } else { <?= '?' ?>>
                 <i class="fa fa-th"></i>
