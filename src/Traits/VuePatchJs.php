f06ab2c545d39149a2ca733150996f72d21b5416
夏琳泰 <xltxlm@qq.com>
2017-03-08 19:17:48 +0800

    * 2017-03-08 添加列表形式+搜索框的vuejs补丁

---
+++ src/Traits/VuePatchJs.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialintai
+ * Date: 2017/3/8
+ * Time: 18:32
+ */
+
+namespace kuaigeng\pushconfig\vendor\xltxlm\h5skin\src\Traits;
+
+use xltxlm\template\Template;
+
+/**
+ * 提供把列表转换成vue的js
+ * Class VuePatch
+ * @package kuaigeng\pushconfig\vendor\xltxlm\h5skin\src\Traits
+ */
+class VuePatchJs extends Template
+{
+    /** @var string ajax网址地址 */
+    protected $url = "";
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return VuePatchJs
+     */
+    public function setUrl(string $url): VuePatchJs
+    {
+        $this->url = $url;
+        return $this;
+    }
+}
---
+++ src/Traits/VuePatchJs.tpl.php
@@ -0,0 +1,28 @@
+<?php /** @var  \kuaigeng\pushconfig\vendor\xltxlm\h5skin\src\Traits\VuePatchJs $this */ ?>
+
+<script>
+    var vueel = (new Vue({
+            el: "#vueel",
+            data: {
+                alldata: []
+            },
+            methods: {
+                SearchDo: function () {
+                    var taget = $(event.currentTarget);
+                    if (taget.val() != "") {
+                        $.ajax({
+                            dataType: "json",
+                            method: "GET",
+                            url: '<?=$this->getUrl()?>',
+                            data: {keyword: taget.val()},
+                            success: function (result) {
+                                vueel.$data.alldata = result;
+                            }
+                        });
+                    }
+                }
+
+            }
+        })
+    );
+</script>
