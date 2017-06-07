a6568f37bd0187bd7015e7bdbb4d457c8b6e108c
夏琳泰 <xltxlm@qq.com>
2017-06-07 15:51:30 +0800

    * 2017-06-07 添加多选操作

--- src/Traits/VueLink.tpl.php
+++ src/Traits/VueLink.tpl.php
@@ -20,9 +20,9 @@ use xltxlm\h5skin\Request\UserCookieModelCopy; ?>
         modelname ='';
     }
     //字段关联外表对照关系
-    if(typeof Field2table == 'undefined')
+    if(typeof Field2tables == 'undefined')
     {
-        Field2table =[];
+        Field2tables =[];
     }
     var oldgetdata=[];
     var <?=$this::vueel()?> =new Vue({
@@ -130,62 +130,70 @@ use xltxlm\h5skin\Request\UserCookieModelCopy; ?>
     <?=$this::vueel()?>. action();
 
     //管辖之内,数据变动，提交上传
-    eval('watchmodel=<?=$this::vueel()?>.$data.alldata.'+modelname);
-    watchmodel.forEach(function (item,index) {
-        for (var tagname in item) {
-            //如果是外关联别的表格，那么在本表写入格式的明文的json字符串
-            if($.inArray(tagname,Field2tables) != -1)
-            {
-                if(item[tagname][0]=='[')
-                    item[tagname]=JSON.parse(item[tagname]);
-                else
-                    item[tagname]=[];
-            }
-            //绑定操作
-            <?=$this::vueel()?>.$watch("alldata."+modelname+'.'+index+"."+tagname,function (newVal, oldVal) {
-                //如果没标志要修改的字段名字。退出.
-                if(!this.openedittagname)
+    eval('watchmodel={object:<?=$this::vueel()?>.$data.alldata.'+modelname+',name:"'+modelname+'"}');
+    eval('watchmodeled={object:<?=$this::vueel()?>.$data.alldata.'+modelname+'ed,name:"'+modelname+'ed"}');
+    [watchmodel,watchmodeled].forEach(function(watchmodelItem,itemindex) {
+        watchmodelItem.object.forEach(function (item,index) {
+            for (var tagname in item) {
+                //如果是外关联别的表格，那么在本表写入格式的明文的json字符串
+                if($.inArray(tagname,Field2tables) != -1)
                 {
-                    return false;
+                    if(item[tagname][0]=='[')
+                        item[tagname]=JSON.parse(item[tagname]);
+                    else
+                        item[tagname]=[];
                 }
-                if(typeof newVal =='object')
-                {
-                    newVal=JSON.stringify(newVal);
-                }
-                //发送数据,编辑当前字段的值
-                $.ajax({
-                    dataType: "json",
-                    method: "GET",
-                    url: '<?=$this->getEditAjaxUrl()?>',
-                    data: {
-                        'id':this.openeditflag,
-                        'name':this.openedittagname,
-                        'value':newVal
-                    },
-                    success: function (result) {
-                        if($.inArray(<?=$this::vueel()?>.$data.openedittagname,<?=$this::vueel()?>.$data.reloadfield) != -1)
-                        {
-                            //如果带有多选下拉框，只能刷新页面了
-                            if([].toString()==Field2tables.toString())
-                            {
-                                <?=$this::vueel()?>.action();
-                                ajaxSuccess(result,<?=$this::vueel()?>.$data.openeditiitem);
-                            }else
-                            {
-                                window.location.reload();
-                            }
-                        }
-                    },
-                    error:function (XMLHttpRequest,textStatus) {
-                        ajaxError(textStatus,<?=$this::vueel()?>.$data.openeditiitem)
-                    },
-                    complete:function (XMLHttpRequest, textStatus) {
-                        <?=$this::vueel()?>.$data.openedittagname='';
+                console.log('alldata.'+watchmodelItem.name+'.'+index+"."+tagname)
+                console.log("->alldata."+modelname+'.'+index+"."+tagname)
+                //绑定操作
+                <?=$this::vueel()?>.$watch('alldata.'+watchmodelItem.name+'.'+index+"."+tagname,function (newVal, oldVal) {
+                //<?=$this::vueel()?>.$watch("alldata."+modelname+'.'+index+"."+tagname,function (newVal, oldVal) {
+                    console.log("有数据发生改变.");
+                    //如果没标志要修改的字段名字。退出.
+                    if(!this.openedittagname)
+                    {
+                        console.log("没有指明标记的标签名字.");
+                        return false;
                     }
+                    if(typeof newVal =='object')
+                    {
+                        newVal=JSON.stringify(newVal);
+                    }
+                    //发送数据,编辑当前字段的值
+                    $.ajax({
+                        dataType: "json",
+                        method: "GET",
+                        url: '<?=$this->getEditAjaxUrl()?>',
+                        data: {
+                            'id':this.openeditflag,
+                            'name':this.openedittagname,
+                            'value':newVal
+                        },
+                        success: function (result) {
+                            if($.inArray(<?=$this::vueel()?>.$data.openedittagname,<?=$this::vueel()?>.$data.reloadfield) != -1)
+                            {
+                                //如果带有多选下拉框，只能刷新页面了
+                                if([].toString()==Field2tables.toString())
+                                {
+                                    <?=$this::vueel()?>.action();
+                                    ajaxSuccess(result,<?=$this::vueel()?>.$data.openeditiitem);
+                                }else
+                                {
+                                    window.location.reload();
+                                }
+                            }
+                        },
+                        error:function (XMLHttpRequest,textStatus) {
+                            ajaxError(textStatus,<?=$this::vueel()?>.$data.openeditiitem)
+                        },
+                        complete:function (XMLHttpRequest, textStatus) {
+                            <?=$this::vueel()?>.$data.openedittagname='';
+                        }
+                    });
                 });
-            });
 
-        }
+            }
+        })
     });
 
     //管辖之内,有name的元素,全部加上联动绑定
